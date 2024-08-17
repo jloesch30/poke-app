2ecd4eb614ef08544b0b6b6bd96f6c8be7fb589e
Josh Loesch <josh.loesch@strongholdim.com>
2024-08-17 12:53:31 -0500

    Home page styling

--- client/src/pages/home/page.tsx
+++ client/src/pages/home/page.tsx
@@ -1,18 +1,58 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './page.module.scss';
-import { Container } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text, Link } from '@chakra-ui/react';
+import { routes } from 'src/routes';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h1 className={styles.header}>
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      minWidth="100vw"
+      minHeight="80vh"
+      marginBlock="auto"
+    >
+      <Heading className={styles.header}>
         Welcome to your <span className={styles.fadeIn}>Pokedex</span>
-      </h1>
-      <Container maxW="2xl" className={styles.container}>
-        <p>This project is built with the following technologies:</p>
-        <Link to="/pokedex">Enter your Pokedex</Link>
-      </Container>
-    </>
+      </Heading>
+      <Flex
+        maxW="2xl"
+        className={styles.container}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text marginTop="4">
+          Welcome to the Pokedex app! Please feel free to explore and interact
+          with your favorite Pokemon. This app is built with{' '}
+          <span className={styles.emphasis}>Vite</span>,{' '}
+          <span className={styles.emphasis}>React</span>,{' '}
+          <span className={styles.emphasis}>TypeScript</span>,{' '}
+          <span className={styles.emphasis}>Chakra UI</span>, and{' '}
+          <span className={styles.emphasis}>Jotai</span>.
+        </Text>
+        <Text marginTop="1" display='flex' flexDirection='row' gap='10px'>
+          Created by:{' '}
+          <Link
+            href="https://github.com/jloesch30"
+            isExternal
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="center"
+          >
+            Josh Loesch <ExternalLinkIcon mx="2px" marginLeft='5px' />
+          </Link>
+        </Text>
+        <Button onClick={() => navigate(routes.pokeDexPath)}>
+          Go to Pokedex
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
